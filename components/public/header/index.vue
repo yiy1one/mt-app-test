903410c3d8d4e23c90e3c9aa1a1597be4f518174
yiy1one <yiy1one@163.com>
2019-04-23 08:22:56 +0800

    first commit

---
+++ components/public/header/index.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <el-row :gutter="15" calss="m-header">
+    <el-col :span="6">
+      <Geo/>
+    </el-col>
+    <el-col :span="6"></el-col>
+    <el-col :span="6"></el-col>
+  </el-row>
+
+</template>
+
+<script>
+import Geo from './geo'
+import Nav from './nav'
+export default {
+  components:{
+    Geo
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "@/assets/css/public/layout.scss";
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-container>
+    <el-header>
+      <my-header/>
+    </el-header>
+    <el-main>
+      <nuxt/>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import MyHeader from '@/components/public/header'
+export default {
+  components:{
+    MyHeader
+  }
+}
+</script>
+
+
+<style>
+
+</style>
